4f905d7714a3a3e4a59fd5444c3ae55a1b027d0a
徐骏腾 <2562187986@qq.com>
2026-07-08 19:50:23 +0800

    Delete api.md directory

--- api.md/src/main/java/com/liveassistant/voiceai/service/AudioStorageService.java
+++
@@ -1,7 +0,0 @@
-package com.liveassistant.voiceai.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface AudioStorageService {
-    String saveAudio(MultipartFile file, Long userId) throws Exception;
-}
--- api.md/src/main/java/com/liveassistant/voiceai/service/TtsProvider.java
+++
@@ -1,7 +0,0 @@
-package com.liveassistant.voiceai.service;
-
-import com.liveassistant.voiceai.dto.TtsConvertRequest;
-
-public interface TtsProvider {
-    byte[] synthesize(TtsConvertRequest request, String voiceParams) throws Exception;
-}
